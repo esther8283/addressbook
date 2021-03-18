33a9c859c4bd9e17b49d3ec4c517be7dbe3ad371
esther8283 <esther8283@naver.com>
2021-03-18 17:34:27 +0900

    Dto

---
+++ src/main/java/address/dto/AddressDto.java
@@ -0,0 +1,14 @@
+package address.dto;
+
+import lombok.Data;
+
+@Data
+public class AddressDto {
+	private int num;
+	private String name;
+	private String phone;
+	private String tel;
+	private String email;
+	private String addr;
+
+}
