c8746885bd7bc46437d65aa14676a6f8e6e89679
esther8283 <esther8283@naver.com>
2021-03-18 17:34:38 +0900

    Mapper

---
+++ src/main/java/address/mapper/AddressMapper.java
@@ -0,0 +1,18 @@
+package address.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import address.dto.AddressDto;
+
+@Mapper
+public interface AddressMapper {
+
+	List<AddressDto> selectAddressList() throws Exception;
+	
+	AddressDto selectAddressDetail(int num) throws Exception;
+	
+	void insertAddress(AddressDto address) throws Exception;
+	
+}
